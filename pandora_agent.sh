ca816fb738a8167c205a3ad313509764299870c6
PedroAcevedo <pdacevedo@uninorte.edu.co>
2018-11-05 18:47:40 -0500

    Add files via upload

---
+++ pandora_agent.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+# Make sure only root can run the script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo "---------------------Pedro Acevedo The ScriptingMan----------------------------"
+#download pandora agent for CentOS/RHEL
+wget -O pandorafms_agent_unix-7.0NG.728-1.noarch.rpm https://sourceforge.net/projects/pandora/files/Pandora%20FMS%207.0NG/728/RHEL_CentOS/pandorafms_agent_unix-7.0NG.728-1.noarch.rpm/download
+#intall ignoring dependencies
+yum install pandorafms_agent_unix-7.0NG.728-1.noarch.rpm -y
+#replace localhost for the server ip
+sed -i "s/localhost/52.55.235.188/" /etc/pandora/pandora_agent.conf
+#initialize the service
+echo -ne "[                                                                     ]\r"
+sleep 1
+echo -ne "[==                                                                   ]\r"
+sleep 1
+echo -ne "[====                                                                 ]\r"
+sleep 1
+echo -ne "[=======                                                              ]\r"
+echo -ne "[===========                                                          ]\r"
+echo -ne "[=================                                                    ]\r"
+echo -ne "[=======================                                              ]\r"
+echo -ne "[============================                                         ]\r"
+sleep 1
+echo -ne "[===================================                                  ]\r"
+sleep 1
+echo -ne "[===========================================                          ]\r"
+echo -ne "[================================================                     ]\r"
+echo -ne "[===================================================                  ]\r"
+echo -ne "[========================================================             ]\r"
+sleep 1
+echo -ne "[==========================================================           ]\r"
+sleep 1
+echo -ne "[=============================================================        ]\r"
+echo -ne "[================================================================     ]\r"
+echo -ne "[==================================================================   ]\r"
+sleep 1
+echo -ne "[===================================================================  ]\r"
+sleep 2
+echo -ne "[=====================================================================]\r"
+echo -ne '\n'
+/etc/init.d/pandora_agent_daemon restart
+echo "---------------------------End----------------------------------------"
