211458fec44a5973ce7bef9012bbca70ff82deb9
PedroAcevedo <pdacevedo@uninorte.edu.co>
2018-11-06 09:10:24 -0500

    Add files via upload

--- pandora_agent.sh
+++ pandora_agent.sh
@@ -9,11 +9,11 @@ if [ "$(id -u)" != "0" ]; then
 fi
 echo "---------------------Pedro Acevedo The ScriptingMan----------------------------"
 #download pandora agent for CentOS/RHEL
-wget -O pandorafms_agent_unix-7.0NG.728-1.noarch.rpm https://sourceforge.net/projects/pandora/files/Pandora%20FMS%207.0NG/728/RHEL_CentOS/pandorafms_agent_unix-7.0NG.728-1.noarch.rpm/download
+wget -e https_proxy=172.17.3.10:8080 -O pandorafms_agent_unix-7.0NG.728-1.noarch.rpm https://sourceforge.net/projects/pandora/files/Pandora%20FMS%207.0NG/728/RHEL_CentOS/pandorafms_agent_unix-7.0NG.728-1.noarch.rpm/download -P .
 #intall ignoring dependencies
 yum install pandorafms_agent_unix-7.0NG.728-1.noarch.rpm -y
 #replace localhost for the server ip
-sed -i "s/localhost/52.55.235.188/" /etc/pandora/pandora_agent.conf
+sed -i "s/localhost/$1/" /etc/pandora/pandora_agent.conf
 #initialize the service
 echo -ne "[                                                                     ]\r"
 sleep 1
